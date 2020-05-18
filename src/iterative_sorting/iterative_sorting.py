77da3efa1a7b4041aefba2f38c22e9fb34a029dc
Adam <adammonast@Adams-MBP.attlocal.net>
2020-05-18 16:21:33 -0400

    iterative_searching complete

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,16 +1,36 @@
 # TO-DO: Complete the selection_sort() function below
 def selection_sort(arr):
     # loop through n-1 elements
-    for i in range(0, len(arr) - 1):
-        cur_index = i
-        smallest_index = cur_index
-        # TO-DO: find next smallest element
-        # (hint, can do in 3 loc)
-        # Your code here
+    # for i in range(0, len(arr) - 1):
+    #     cur_index = i
+    #     smallest_index = cur_index
+    # TO-DO: find next smallest element
+    # (hint, can do in 3 loc)
+    # Your code here
 
-        # TO-DO: swap
-        # Your code here
+    # TO-DO: swap
+    # Your code here
 
+    # set first number as the minimum, then compare to every number towards the right
+    # when a higher number is found, that become the new minimum and process repeats
+    # minimum value gets moved to the left
+    # left side becomes sorted, right becomes unsorted
+
+    # use -1 so at the lasy iteration, the last number remaining becomes the newest minimum value
+    indexing_length = range(0, len(arr) - 1)
+
+    for i in indexing_length:
+        # set i to min value so the first item in the sorted list becomes the min value every iteration
+        min_value = i
+        # i + 1 for all elements to the right of the min value
+        for j in range(i + 1, len(arr)):
+            # replace min value if element to the right is greater than min value
+            if arr[j] < arr[min_value]:
+                min_value = j
+        # if we find a lower than min, switch those values
+        if min_value != i:
+            arr[min_value], arr[i] = arr[i], arr[min_value]
+    # return array
     return arr
 
 
@@ -28,7 +48,6 @@ def bubble_sort(arr):
             if arr[i] > arr[i + 1]:
                 sorted = False
                 arr[i], arr[i + 1] = arr[i + 1], arr[i]
-
     # when the list gets sorted, the while loop will not trigger, keeping sorted to True
     return arr
 
